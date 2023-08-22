ab45afc5c8f6e914af1b5012ebaacaa23db41071
Vasav Prashar <prashar.vasav@tftus.com>
2023-08-22 19:04:52 +0530

    Section 6 travel list

---
+++ travel-list-app/src/App.js
@@ -0,0 +1,68 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Pants", quantity: 1, packed: true },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>✈Far Away</h1>;
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3> What do you need for your trip</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Enter Your Item Here..." />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id}/>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item(props) {
+  return (
+    <li>
+      <span style={props.item.packed ? { textDecoration: "line-through" } : {}}>
+        {props.item.quantity} {props.item.description}
+      </span>
+      <button style={{ color: "red" }}>&times;</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      You have x items in your list and you already packed (x%)
+    </footer>
+  );
+}
