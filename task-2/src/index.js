17f159826167b4b7222bce09d1bb88266c39b721
Vasav Prashar <prashar.vasav@tftus.com>
2023-10-05 15:22:27 +0530

    added english and hindi language translations

--- task-2/src/index.js
+++ task-2/src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import "./i18n/config";
 import "@fontsource/montserrat";
+import SelectLanguage from "./components/SelectLanguage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <SelectLanguage/>
       <App />
     </Provider>
   </React.StrictMode>
